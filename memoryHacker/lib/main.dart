373de70f05f60a9b7b1edf60fadcdad38bc07797
Bryan Calisto <bryancalisto@yahoo.es>
2022-11-14 21:45:33 -0500

    Updated title

--- memoryHacker/lib/main.dart
+++ memoryHacker/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pinball Space Cadet Memory hacker',
+      title: 'Pinball MemoryHacker',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
