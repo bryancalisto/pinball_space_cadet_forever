a9af7ecfc4fd5c57eaa84e0488436460c02d9c6a
Bryan Calisto <bryancalisto@yahoo.es>
2026-06-02 17:06:40 -0500

    cleaning and adding images

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 mkdir -Force build
-cl /EHsc /std:c++20 /Fe:build/pinball-hacker.exe src/main.cpp /link psapi.lib user32.lib kernel32.lib
+cl /EHsc /std:c++20 /Fe:build/pinball-hacker.exe src/main.cpp /link psapi.lib user32.lib
